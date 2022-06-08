98548f443eda11f2e221eb89024827ab6276233b
Lisa Medina <lisa.medinag@gmail.com>
2022-06-08 13:58:11 +0200

    initialized project

---
+++ src/main/java/com/ironhack/bodyandmindserver/BodyAndMindServerApplication.java
@@ -0,0 +1,13 @@
+package com.ironhack.bodyandmindserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BodyAndMindServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BodyAndMindServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ironhack/bodyandmindserver/BodyAndMindServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ironhack.bodyandmindserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BodyAndMindServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
