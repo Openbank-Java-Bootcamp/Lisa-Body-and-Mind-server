213774641b6cb3cc65798a56e70d39f765f94e26
Lisa Medina <lisa.medinag@gmail.com>
2022-06-10 15:43:06 +0200

    fixed program/exerciseType have unique name, for each user

--- training-service/src/main/java/com/ironhack/trainingservice/utils/Utils.java
+++ training-service/src/main/java/com/ironhack/trainingservice/utils/Utils.java
@@ -3,13 +3,14 @@ package com.ironhack.trainingservice.utils;
 import com.ironhack.trainingservice.enums.Creator;
 import com.ironhack.trainingservice.repository.ExerciseTypeRepository;
 import com.ironhack.trainingservice.repository.ProgramRepository;
-import com.ironhack.trainingservice.repository.WorkoutRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ResponseStatusException;
 
 @Component
+@Slf4j
 public class Utils {
 
     @Autowired
@@ -18,28 +19,31 @@ public class Utils {
     @Autowired
     private ExerciseTypeRepository exerciseTypeRepository;
 
-    public void validateProgramNameIsUnique(String name, Integer userId){
-        if(userId!=null)
-        if (programRepository.existsProgramByUserIdAndName(userId,name)
-                &&programRepository.existsProgramByCreatorAndName(Creator.TRAINER, name)) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Unable to create Program, name already exists");
-        }
-        else
-        if (programRepository.existsProgramByCreatorAndName(Creator.TRAINER, name)) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Unable to create Program, name already exists");
+    public void validateProgramNameIsUnique(String name, Integer userId) {
+        if (userId != null) {
+            if (programRepository.existsProgramByUserIdAndName(userId, name)
+                    || programRepository.existsProgramByCreatorAndName(Creator.TRAINER, name)) {
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Unable to create Program, name already exists");
+            }
+        } else {
+            if (programRepository.existsProgramByCreatorAndName(Creator.TRAINER, name)) {
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Unable to create Program, name already exists");
+            }
         }
     }
 
-    public void validateExerciseTypeNameIsUnique(String name, Integer userId){
-        if(userId!=null)
-        if (exerciseTypeRepository.existsExerciseTypeByUserIdAndName(userId, name)
-                &&exerciseTypeRepository.existsExerciseTypeByCreatorAndName(Creator.TRAINER, name)) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Unable to create Exercise Type, name already exists");
-        }
-
-        else
-        if (exerciseTypeRepository.existsExerciseTypeByCreatorAndName(Creator.TRAINER, name)) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Unable to create Exercise Type, name already exists");
+    public void validateExerciseTypeNameIsUnique(String name, Integer userId) {
+        log.info("------------------"+name+" "+userId);
+        log.info(exerciseTypeRepository.existsExerciseTypeByUserIdAndName(userId, name).toString());
+        if (userId != null) {
+            if (exerciseTypeRepository.existsExerciseTypeByUserIdAndName(userId, name)
+                    || exerciseTypeRepository.existsExerciseTypeByCreatorAndName(Creator.TRAINER, name)) {
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Unable to create Exercise Type, name already exists");
+            }
+        } else {
+            if (exerciseTypeRepository.existsExerciseTypeByCreatorAndName(Creator.TRAINER, name)) {
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Unable to create Exercise Type, name already exists");
+            }
         }
     }
 }
