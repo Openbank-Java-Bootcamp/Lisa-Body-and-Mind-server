5ad5ea3ff48ac83178370c2767c1d8de7e267fa8
Lisa Medina <lisa.medinag@gmail.com>
2022-06-11 12:11:21 +0200

    done api gateway and cors config

--- api-gateway/src/main/java/com/ironhack/apigateway/GatewayConfiguration.java
+++ api-gateway/src/main/java/com/ironhack/apigateway/GatewayConfiguration.java
@@ -8,23 +8,28 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class GatewayConfiguration {
 
-//    @Bean
-//    public RouteLocator gatewayRouter(RouteLocatorBuilder builder){
-//        return builder.routes()
-//
-//                // Movie Info Service
-//                .route(p -> p.path("/api/movies/**")
-//                        .uri("lb://MOVIE-INFO-SERVICE"))
-//
-//                //Ratings data service
-//                .route(p -> p.path("/api/ratings**")
-//                        .uri("lb://RATINGS-DATA-SERVICE"))
-//                .route(p -> p.path("/api/users/**")
-//                        .uri("lb://RATINGS-DATA-SERVICE"))
-//
-//                .route(p -> p.path("/api/catalogs/**")
-//                        .uri("lb://MOVIE-CATALOG-SERVICE"))
-//
-//                .build();
-//    }
+    @Bean
+    public RouteLocator gatewayRouter(RouteLocatorBuilder builder){
+        return builder.routes()
+
+                .route(p -> p.path("/api/exercises/**")
+                        .uri("lb://TRAINING-SERVICE"))
+                .route(p -> p.path("/api/exercise-sessions/**")
+                        .uri("lb://TRAINING-SERVICE"))
+                .route(p -> p.path("/api/exercise-types/**")
+                        .uri("lb://TRAINING-SERVICE"))
+                .route(p -> p.path("/api/programs/**")
+                        .uri("lb://TRAINING-SERVICE"))
+                .route(p -> p.path("/api/repetitions/**")
+                        .uri("lb://TRAINING-SERVICE"))
+                .route(p -> p.path("/api/sets/**")
+                        .uri("lb://TRAINING-SERVICE"))
+                .route(p -> p.path("/api/workouts/**")
+                        .uri("lb://TRAINING-SERVICE"))
+
+                .route(p -> p.path("/api/users/**")
+                        .uri("lb://USER-SERVICE"))
+
+                .build();
+    }
 }
