9d3097728dffd150f73025265ec3b94e37b83159
Lisa Medina <lisa.medinag@gmail.com>
2022-06-11 21:22:19 +0200

    changed allowed methods settings cors

--- training-service/src/main/java/com/ironhack/trainingservice/DTOs/ProgramDto.java
+++ training-service/src/main/java/com/ironhack/trainingservice/DTOs/ProgramDto.java
@@ -2,11 +2,15 @@ package com.ironhack.trainingservice.DTOs;
 
 import lombok.*;
 
+import javax.validation.constraints.NotNull;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 public class ProgramDto {
+    @NotNull(message = "program name must be defined")
     private String name;
+    @NotNull(message = "creator field must be defined")
     private String creator;
     private Integer userId;
 }
